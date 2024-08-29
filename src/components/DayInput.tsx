2475561e54b4557a2ef4cb2883d8c3f432a98c22
Ted Jenkler <tedjenkler@gmail.com>
2024-08-29 13:09:33 +0200

    Feat: Add and style DayInput component

--- src/components/DayInput.tsx
+++ src/components/DayInput.tsx
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
+
 function DayInput() {
+    const [day, setDay] = useState<string>('');
+
+    const handleDay = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        const numbervalue = Number(value);
+        if(!isNaN(numbervalue) && numbervalue <= 31) {
+            setDay(value);   
+        }
+    };
+
   return (
-    <div>
-      
+    <div className="input">
+      <label>DAY</label>
+      <input onChange={handleDay} value={day} placeholder="0">
+      </input>
     </div>
   )
 }
