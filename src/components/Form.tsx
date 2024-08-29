1a4832d34cbbf8d55958429c99986054f9aaef8a
Ted Jenkler <tedjenkler@gmail.com>
2024-08-29 17:36:53 +0200

    Style: Improve mobile layout for AgeCalc component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,23 +2,20 @@ import DayInput from "./DayInput";
 import MonthInput from "./MonthInput";
 import YearInput from "./YearInput";
 import arrow from "../assets/images/arrow.png";
+import Info from "./Info";
 
 function Form() {
   return (
     <form>
-      <div>
+      <div className="inputs">
         <DayInput />
         <MonthInput />
         <YearInput />
-        <button>
+      </div>
+      <button className="button">
             <img src={arrow} alt="arrow"/>
         </button>
-      </div>
-      <div>
-        <p><span>38</span>years</p>
-        <p><span>3</span>months</p>
-        <p><span>26</span>days</p>
-      </div>
+      <Info />
     </form>
   )
 }
---
+++ src/components/Info.tsx
@@ -0,0 +1,11 @@
+function Info() {
+  return (
+    <div className="info">
+        <p><span>38 </span>years</p>
+        <p><span>3 </span>months</p>
+        <p><span>26 </span>days</p>
+    </div>
+  )
+}
+
+export default Info
